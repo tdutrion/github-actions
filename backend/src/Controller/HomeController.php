fdf9851b8d4c728bd7e6c0c40982bf1508e01dbf
Thomas Dutrion <thomas@engineor.com>
2024-05-15 23:05:37 +0200

    Add datetime information to homepage

--- backend/src/Controller/HomeController.php
+++ backend/src/Controller/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Component\Clock\ClockInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,9 +17,16 @@ final readonly class HomeController
     private const ROUTE_NAME = 'app_home';
     private const ROUTE_PATH = '/';
 
+    public function __construct(
+        private ClockInterface $clock
+    ) {}
+
     #[Route(path: self::ROUTE_PATH, name: self::ROUTE_NAME, methods: [Request::METHOD_GET])]
     public function __invoke(): JsonResponse
     {
-        return new JsonResponse(data: ['success' => true], status: Response::HTTP_OK);
+        return new JsonResponse(data: [
+            'success' => true,
+            'datetime' => $this->clock->now(),
+        ], status: Response::HTTP_OK);
     }
 }
